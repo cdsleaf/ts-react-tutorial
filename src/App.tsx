600da816899082a4c333e99a6c59a3ac3a912262
cdsleaf <cdsleaf@gmail.com>
2019-11-14 23:46:09 +1300

    feat: add counter/MyForm/ReducerSample components

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import './App.css';
 import Greetings from './Greetings';
 import Counter from './Counter';
+import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
+  const onSubmit = (form: { name: string; description: string}) => {
+    console.log(form);
+  };
   return (
     <div className="App">
+      <p>Greetings</p>
       <Greetings name="eee" onClick={onClick}/>
+      <hr />
+      <p>Counter</p>
       <Counter />
+      <hr />
+      <p>My Form</p>
+      <MyForm onSubmit={onSubmit} />
+      <hr />
+      <p>ReducerSample</p>
+      <ReducerSample />
+      <hr />
     </div>
   );
 }
